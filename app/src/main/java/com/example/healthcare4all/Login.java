742f01c982863a2e9c1411d18ca57521d85a71f3
JessePri <56932258+JessePri@users.noreply.github.com>
2022-12-05 08:50:56 -0600

    Server is connected.

--- app/src/main/java/com/example/healthcare4all/Login.java
+++ app/src/main/java/com/example/healthcare4all/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Base64;
+import java.util.concurrent.TimeUnit;
+import com.google.gson.*;
+
+
+
+import okhttp3.*;
+
+
 public class Login extends AppCompatActivity {
 
     EditText username;
@@ -23,6 +37,10 @@ public class Login extends AppCompatActivity {
     Button button_login;
 
     FirebaseAuth mAuth;
+
+    private static String b64UrlDecode(String encodedString) {
+        return new String(Base64.getUrlDecoder().decode(encodedString));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +67,66 @@ public class Login extends AppCompatActivity {
                 String email = username.getText().toString();
                 String password = enter_password.getText().toString();
 
-                login(email, password);
+//                login(email, password);
+
+                // DEMO FOR HOW THIS WORKS
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+                Log.i("##############1111", "TEST");
+
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .connectTimeout(15, TimeUnit.SECONDS)
+//                        .retryOnConnectionFailure(true)
+                        .build();
+
+                String json = "{\n" +
+                        "  \"userName\": \"pat\",\n" +
+                        "  \"password\": \"string\",\n" +
+                        "  \"privilege\": 1\n" +
+                        "}";
+
+//                String json = "{\"Reason\": \"" + "test" + "\"}";
+
+                RequestBody body = RequestBody.create(json, JSON);
+
+                Log.i("##############1111", "TEST");
+
+                Request request = new Request.Builder()
+                        .url("http://10.0.2.2:5253/Login")
+                        .post(body)
+                        .header("Connection", "close")
+//                        .header("Accept-Encoding", "identity")
+                        .build();
+
+                Log.i("##############1111", "TEST");
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override public void onFailure(Call call, IOException e) {
+                        Log.i("Error", "Unlucky1", e);
+                    }
+
+                    @Override public void onResponse(Call call, Response response) throws IOException {
+                        try (ResponseBody responseBody = response.body()) {
+                            if (!response.isSuccessful())
+                                Log.i("Error", "Unlucky2");
+
+                            Headers responseHeaders = response.headers();
+                            for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                                Log.i("Response", responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                            }
+
+                            String encodedJwt = response.body().string();
+                            String[] splitJWT = encodedJwt.split("\\.");
+
+                            Log.i("encodedJwt", encodedJwt);
+                            Log.i("Body", "" + b64UrlDecode(splitJWT[1]));
+                        }
+                    }
+                });
+
+                Log.i("##############2222", "TEST2");
+
+
             }
         });
 
